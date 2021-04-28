7302eae47bd49cc2616abdac68a6b0ba59522a31
GersonST <gersonsevero90@gmail.com>
2021-04-28 11:02:32 -0300

    Aula Operações

--- script.js
+++ script.js
@@ -13,11 +13,63 @@
 // undefined
 // Number
 // String
-var b = 22;
-var a = 1;
+// var b = 22;
+// var a = 1;
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
-console.log(a + b)
+// console.log(a + b)
 
+
+/* + Adição
+   -Subtração
+   *Multiplicação
+   /Divisão
+   %Módulo
+   ++Incremento
+   --Decremento */
+
+   var a = 10;
+   var b = 15;
+
+    // Soma
+   var c = a + b;
+
+   console.log(c);
+   
+//    Subtração
+   var c = a - b;
+
+   console.log(c);
+
+
+//    Multiplicação
+   var c = a * b;
+
+   console.log(c);
+
+//    Divisão
+   var c = a / b;
+
+   console.log(c);
+
+//    Módulo (Resto da divisão)
+
+   var c = b % a;
+
+   console.log(c);
+
+
+// Incremento
+   var c = ++a;
+
+   console.log(c);
+
+   //    Decremento
+
+   var c = --a;
+
+   console.log(c);
+
+   
