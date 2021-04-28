73bf7ca93d1bb11d3bed1543cf1d0acdcbed8efc
GersonST <gersonsevero90@gmail.com>
2021-04-28 10:37:14 -0300

    Aula Tipos de dados

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-var a;
-a = "Placa de Captura";
-console.log(a + " Ell Gato");
+//Variaveis
+
+// var a;
+// a = "Placa de Captura";
+// console.log(a + " Ell Gato");
+// alert('Sei la ahhauhauh')
+
+
+//Tipos de Dados
+
+// Boolean
+// null
+// undefined
+// Number
+// String
+var b = 22;
+var a = 1;
+
+console.log(a);
+console.log(b);
+
+console.log(a + b)
+
