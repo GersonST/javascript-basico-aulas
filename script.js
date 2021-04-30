5e3c65880da67b7decba776113fcf38f472b0de5
GersonST <gersonsevero90@gmail.com>
2021-04-29 21:58:15 -0300

    Aula Objeto - Método

--- script.js
+++ script.js
@@ -211,4 +211,30 @@
 
 /* Objetos */
 
-var aluno = { nome: "Igor", nota1:7.5 };
+
+
+function calcMedia () {
+    return (this.notas[0] + this.notas[1]) /2
+}
+
+
+var aluno = { nome: "Igor", notas:[7,8],
+
+media: calcMedia
+
+};
+
+
+
+var aluno1 = { nome: "João", notas:[6,8],
+
+media: calcMedia
+
+};
+
+
+console.log(aluno.nome)
+console.log(aluno.media())
+
+console.log(aluno1.nome)
+console.log(aluno1.media())
