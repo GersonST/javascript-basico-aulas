200f3a113e703d84c3f8b72fee18ae5107f7221b
GersonST <gersonsevero90@gmail.com>
2021-04-30 02:01:46 -0300

    Aula Const, Let e Var Final

--- script.js
+++ script.js
@@ -275,6 +275,17 @@
 /* Datas */
 
 
-var d = new Date(0);
+// var d = new Date(0);
 
-console.log(d);
+// console.log(d);
+
+
+/* Const, Let e Var */
+
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = 6;
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
